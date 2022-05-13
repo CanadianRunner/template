55f2c54dc8df686d30ac9cb89f3f340c590203dc
Sean Keane <seankeane93@gmail.com>
2022-05-12 19:52:26 -0700

    update finalized template

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@
 // user interface logic.
 // Import JS/CSS files here.
 // ie: import { PeanutButter } from './peanut-butter.js'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
